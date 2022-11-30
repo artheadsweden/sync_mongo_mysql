817f5396c5eb2b6968b894b5b475451d50721378
Joakim Wassberg <joakim.wassberg@svenskaspel.se>
2022-11-30 08:53:32 +0100

    Added test data

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from mongo_doc import init_db, create_collection_class
 from raw_mysql import RawSql
+import json
+
+
+def get_data(file_name):
+    return json.loads(open(file_name, 'r', encoding='utf-8').read())
+
 
 def test_mongo():
     init_db('mongodb://localhost:27017', 'db')
     DataDoc = create_collection_class('DataDoc', 'data')
-    data = {
-        'name': 'Filias', 
-        'age': 56
-    }
-    doc = DataDoc(**data)
-    doc.save('id', 'auto_1')
+    for data in get_data('test_data.json'):
+        doc = DataDoc(**data)
+        doc.save('id', 'auto_1')
 
     docs = DataDoc.all()
     for d in docs:
@@ -18,10 +21,9 @@ def test_mongo():
 
 def test_mysql():
     sql = RawSql('root', 's3cr37', 'localhost', 3306, 'db')
-    sql.store_dict('id_test', {
-        'name': 'Fia-Lisa',
-        'age': 55
-    })
+    for data in get_data('test_data.json'):
+        sql.store_dict('id_test', data)
+
     for row in sql.get_all('id_test'):
         print(row)
 
@@ -29,7 +31,7 @@ def main():
     print('MongoDB')
     print('*' * 40)
     test_mongo()
-    print('MySQL')
+    print('\nMySQL')
     print('*' * 40)
     test_mysql()
 
